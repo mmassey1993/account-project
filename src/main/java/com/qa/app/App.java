d9ae23bd855ef936042673ab1b46c26f15bfe994
mmassey1993 <michaelmassey100@gmail.com>
2018-07-10 10:14:30 +0100

    saved main file

--- src/main/java/com/qa/app/App.java
+++ src/main/java/com/qa/app/App.java
@@ -3,7 +3,7 @@ package com.qa.app;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello world to the standard out");
 
 	}
 
